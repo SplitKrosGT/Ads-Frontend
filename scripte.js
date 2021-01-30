45b3a37ec3fa45851c1a379d18661e6b46cb245e
SplitKrosGT <alber5tsadofficial@gmail.com>
2021-01-30 10:08:14 +0500

    save 2324

--- scripte.js
+++ scripte.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
 
     $("#add").click(function() {
-        console.log(1);
+        
+        $("#modal").show();
     });
+
+    $("#close").click(function() {
+        
+        $("#modal").hide();
+    });
+
+
+
+
+
+    $("#new").click(function() {
+       
+        var ad = {
+            text: $("input[name='text']").val(),
+            contactName: $("input[name='name']").val(),
+            contactPhone: $("input[name='phone']").val()
+        }
+    
+        var adHTML = 
+        '<div class="Ads__box">' +
+            '<div class="Box__info">' + ad.text + '</div>' +
+            '<div class="Box__name">' + ad.contactName + '</div>' +
+            '<div class="Box__phone">' + ad.contactPhone + '</div>' +
+        '</div>';
+    
+        $("#content").append(adHTML);
+
+        $("#modal").hide();
+    });
+
 });
